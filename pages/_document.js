49b3bbfc57341b3dc3772ee4c5af7da4f14cffe2
Barbara <barbara.makowiec@wp.pl>
2023-03-31 10:20:02 +0200

    fix budg

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -9,9 +9,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          {/* <meta name="viewport" content="width=device-width,minimum-scale=1, initial-scale=1" /> */}
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
